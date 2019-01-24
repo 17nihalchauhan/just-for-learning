a2bcd827f0a90a56176c25308edbd6e999753ed0
17nihalchauhan <46994450+17nihalchauhan@users.noreply.github.com>
2019-01-24 16:23:46 +0530

    just provides menu

--- hello.c
+++ hello.c
@@ -1,9 +1,17 @@
 #include<stdio.h>
+void menu();
 int main()
 {
  char arr[20];
  printf("Enter your Name: ");
  scanf("%s",&arr);
  printf("Hello %s ",arr);
+ menu();
  return 0;
+}
+void menu()
+{
+ printf("what do you want to perform\n");
+ printf("1.Creating a linkedlist\n2.Traversing a linked list\n");
+ return;
 }
